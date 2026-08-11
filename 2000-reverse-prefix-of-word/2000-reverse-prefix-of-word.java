fa7cac377b5b8fc07fdd069e82bc3867a567411b
Sai praveen bali <saipraveenbali47@gmail.com>
2026-08-11 23:59:42 +0530

    Time: 0 ms (100%), Space: 43.2 MB (22.69%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        int index = word.indexOf(ch);
+        if(index == -1)return word;
+        StringBuilder sb = new StringBuilder(word.substring(0,index + 1));
+        sb.reverse();
+        sb = sb.append(word.substring(index + 1,word.length()));
+        return sb.toString();
+        
+    }
+}
