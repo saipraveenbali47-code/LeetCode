5676da86304e77492a9188e727f68ed67cf7d0a3
Sai praveen bali <saipraveenbali47@gmail.com>
2026-09-08 19:08:55 +0530

    Time: 1 ms (83.65%), Space: 42.4 MB (72.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -4,10 +4,7 @@ class Solution {
             n -= 1000;
             return n + 1;
         }
-        else if(n >= 100000){
-            n -= 100000;
-            return (n + 1) * 2;
-        }
+       
         return 0;
     }
 }
