8bd1ca253ba47836a9b0bb550af6bfdc0dfece14
Sai praveen bali <saipraveenbali47@gmail.com>
2026-09-13 21:24:27 +0530

    Time: 0 ms (100%), Space: 42.3 MB (5.92%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long low = 1, high = num;
+        while(low <= high){
+            long mid = (low + high) / 2;
+            long sq = (mid * mid);
+            if(sq == num) return true;
+            else if(sq < num){
+                low = mid + 1;
+            }
+            else high = mid - 1;
+        }
+        return false;
+    }
+}
